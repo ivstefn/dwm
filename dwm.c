faf3a2a16ebbcf1b46e4b1d76d9625a10cc58942
Ivan Stefanic <ivstefn@sdf-eu.org>
2014-12-11 15:58:05 +0100

    Added bstackhoriz layout as base for bottomdeck layout

--- dwm.c
+++ dwm.c
@@ -160,6 +160,7 @@ static void configurenotify(XEvent *e);
 static void configurerequest(XEvent *e);
 static Monitor *createmon(void);
 static void deck(Monitor *m);
+static void bottomdeck(Monitor *m);
 static void destroynotify(XEvent *e);
 static void detach(Client *c);
 static void detachstack(Client *c);
@@ -673,6 +674,38 @@ deck(Monitor *m) {
 		}
 		else
 			resize(c, m->wx + mw, m->wy, m->ww - mw - (2*c->bw), m->wh - (2*c->bw), False);
+}
+
+void
+bottomdeck(Monitor *m) {
+        int w, mh, mx, tx, ty, th;
+        unsigned int i, n;
+        Client *c;
+
+        for(n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+        if(n == 0)
+                return;
+        if(n > m->nmaster) {
+                mh = m->nmaster ? m->mfact * m->wh : 0;
+                th = (m->wh - mh) / (n - m->nmaster);
+                ty = m->wy + mh;
+        } 
+        else {
+                th = mh = m->wh;
+                ty = m->wy;
+        }
+        for(i = mx = 0, tx = m->wx, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
+                if(i < m->nmaster) {
+                        w = (m->ww - mx) / (MIN(n, m->nmaster) - i);
+                        resize(c, m->wx + mx, m->wy, w - (2 * c->bw), mh - (2 * c->bw), False);
+                        mx += WIDTH(c);
+                } 
+                else {
+                        resize(c, tx, ty, m->ww - (2 * c->bw), th - (2 * c->bw), False);
+                        if(th != m->wh)
+                                ty += HEIGHT(c);
+                }
+        }
 }
 
 void
