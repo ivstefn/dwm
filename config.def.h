0aa2db7962f419e9825719a33d08a8160ec6d9de
Ivan Stefanic <ivstefn@sdf-eu.org>
2014-12-12 18:42:51 +0100

    Mouse3 on taskbar sets monocle layout

--- config.def.h
+++ config.def.h
@@ -116,6 +116,7 @@ static Button buttons[] = {
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkWinTitle,          0,              Button4,        focusstack,     {.i = -1 } },
 	{ ClkWinTitle,          0,              Button5,        focusstack,     {.i = +1 } },
+	{ ClkWinTitle,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          ControlMask,    Button4,        pushup,         {0} },
 	{ ClkWinTitle,          ControlMask,    Button5,        pushdown,       {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
